63e564416ade0ade391371fde4c1afaf0751eb7b
imsongj <imsongj@gmail.com>
2022-12-10 16:22:03 +0900

    style(vendingMachine) : 줄 수정

--- src/main/java/vendingmachine/model/CoinGenerator.java
+++ src/main/java/vendingmachine/model/CoinGenerator.java
@@ -1,9 +1,8 @@
 package vendingmachine.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
-
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,6 +21,13 @@ public class CoinGenerator {
         return coins;
     }
 
+    public LinkedHashMap<Coin, Integer> initializeCoins() {
+        LinkedHashMap<Coin, Integer> coins = new LinkedHashMap<>();
+        Arrays.stream(Coin.values())
+                .forEach(coin -> coins.put(coin, INITIAL_VALUE));
+        return coins;
+    }
+
     public Coin generateRandomCoin(int maxValue) {
         List<Integer> numbers = generateNumberList();
         Coin coin;
@@ -29,13 +35,6 @@ public class CoinGenerator {
             coin = Coin.of(Randoms.pickNumberInList(numbers));
         } while (coin.getAmount() > maxValue);
         return coin;
-    }
-
-    public LinkedHashMap<Coin, Integer> initializeCoins() {
-        LinkedHashMap<Coin, Integer> coins = new LinkedHashMap<>();
-        Arrays.stream(Coin.values())
-                .forEach(coin -> coins.put(coin, INITIAL_VALUE));
-        return coins;
     }
 
     public List<Integer> generateNumberList() {
